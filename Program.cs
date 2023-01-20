36b97841cd88903c563014b8d9380035c0d41a38
nkraseno <nkraseno@emeal.nttdata.com>
2023-01-20 13:33:29 +0100

    Log4Net exercise

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace Log4Net
+{
+    internal class Program
+    {
+        private static readonly ILog Log = Logs.GetLogger();
+        static void Main(string[] args)
+        {
+            var numero = "Hello world!";
+
+            try
+            {
+                int.Parse(numero);
+            }
+            catch(Exception ex)
+            {
+                Log.Error(ex + "\n   " + "Valor del numero a castear a int: " + numero);
+            }
+
+        }
+    }
+}
